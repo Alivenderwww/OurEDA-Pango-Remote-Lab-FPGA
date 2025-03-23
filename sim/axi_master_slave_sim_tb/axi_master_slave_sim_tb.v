9fd3faca3044b0e5dacd6b5f61fac1dae223990f
DLUTdky <1737911955@qq.com>
2025-03-23 16:21:04 +0800

    修改了slave_sim的一些bug

--- sim/axi_master_slave_sim_tb/axi_master_slave_sim_tb.v
+++ sim/axi_master_slave_sim_tb/axi_master_slave_sim_tb.v
@@ -4,23 +4,23 @@ reg clk,rst;
 
 wire [31:0] WR_ADDR      ;
 wire [ 7:0] WR_LEN       ;
-wire [ 1:0] WR_ID        ;
+wire [ 3:0] WR_ID        ;
 wire        WR_ADDR_VALID;
 wire        WR_ADDR_READY;
 wire [31:0] WR_DATA      ;
 wire [ 3:0] WR_STRB      ;
-wire [ 1:0] WR_BACK_ID   ;
+wire [ 3:0] WR_BACK_ID   ;
 wire        WR_DATA_VALID;
 wire        WR_DATA_READY;
 wire        WR_DATA_LAST ;
 wire [31:0] RD_ADDR      ;
 wire [ 7:0] RD_LEN       ;
-wire [ 1:0] RD_ID        ;
+wire [ 3:0] RD_ID        ;
 wire        RD_ADDR_VALID;
 wire        RD_ADDR_READY;
 wire [31:0] RD_DATA      ;
 wire        RD_DATA_LAST ;
-wire [ 1:0] RD_BACK_ID   ;
+wire [ 3:0] RD_BACK_ID   ;
 wire        RD_DATA_READY;
 wire        RD_DATA_VALID;
 initial begin
@@ -36,28 +36,34 @@ axi_master_sim master_inst(
    .rst          (rst          ),
    .WR_ADDR      (WR_ADDR      ),
    .WR_LEN       (WR_LEN       ),
-   .WR_ID        (WR_ID        ),
+   .WR_ID        (WR_ID   [1:0]     ),
    .WR_ADDR_VALID(WR_ADDR_VALID),
    .WR_ADDR_READY(WR_ADDR_READY),
    .WR_DATA      (WR_DATA      ),
    .WR_STRB      (WR_STRB      ),
-   .WR_BACK_ID   (WR_BACK_ID   ),
+   .WR_BACK_ID   (WR_BACK_ID [1:0]  ),
    .WR_DATA_VALID(WR_DATA_VALID),
    .WR_DATA_READY(WR_DATA_READY),
    .WR_DATA_LAST (WR_DATA_LAST ),
    .RD_ADDR      (RD_ADDR      ),
    .RD_LEN       (RD_LEN       ),
-   .RD_ID        (RD_ID        ),
+   .RD_ID        (RD_ID [1:0]       ),
    .RD_ADDR_VALID(RD_ADDR_VALID),
    .RD_ADDR_READY(RD_ADDR_READY),
    .RD_DATA      (RD_DATA      ),
    .RD_DATA_LAST (RD_DATA_LAST ),
-   .RD_BACK_ID   (RD_BACK_ID   ),
+   .RD_BACK_ID   (RD_BACK_ID[1:0]   ),
    .RD_DATA_READY(RD_DATA_READY),
    .RD_DATA_VALID(RD_DATA_VALID)
 );
 
-
+initial begin
+   #10000
+   master_inst.send_wr_addr(32'h00000170, 100, 0);
+   master_inst.send_wr_data(32'h00000000, 100, 4'b1111, 0);
+   master_inst.send_rd_addr(32'h00000170, 100, 0);
+   master_inst.recv_rd_data(0);
+end
 axi_slave_sim slave_inst(
    .clk          (clk          ),
    .rstn         (~rst         ),
@@ -79,9 +85,10 @@ axi_slave_sim slave_inst(
    .RD_ID        (RD_ID        ),
    .RD_ADDR_VALID(RD_ADDR_VALID),
    .RD_ADDR_READY(RD_ADDR_READY),
-   .RD_BACK_ID   (RD_DATA      ),
-   .RD_DATA      (RD_DATA_LAST ),
-   .RD_DATA_LAST (RD_BACK_ID   ),
+   
+   .RD_DATA      (RD_DATA ),
+   .RD_DATA_LAST (RD_DATA_LAST   ),
+   .RD_BACK_ID   (  RD_BACK_ID    ),
    .RD_DATA_READY(RD_DATA_READY),
    .RD_DATA_VALID(RD_DATA_VALID) 
 
---
+++ sim/axi_master_slave_sim_tb/behav/sim_file_list.f
@@ -0,0 +1,4 @@
+../../../AXI4/sim_file/axi_master_sim.v
+../../../AXI4/sim_file/axi_slave_sim.sv
+../axi_master_slave_sim_tb.v
+-sv
