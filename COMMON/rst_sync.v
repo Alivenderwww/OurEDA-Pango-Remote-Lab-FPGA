15a799ac4fd33fbf4336b1a7e5feef01c8a43996
alivender <13898766233@163.com>
2025-03-31 17:35:12 +0800

    修改了rstn_sync模块的bug

--- COMMON/rst_sync.v
+++ COMMON/rst_sync.v
@@ -6,11 +6,11 @@ module rstn_sync(
 
 reg rstn_d0, rstn_d1;
 always @(posedge clk or negedge rstn_async) begin
-    if(!rst_async) rstn_d0 <= 1'b0;
+    if(!rstn_async) rstn_d0 <= 1'b0;
     else rstn_d0 <= 1'b1;
 end
-always @(posedge clk or negedge rst_async) begin
-    if(!rst_async) rstn_d1 <= 1'b0;
+always @(posedge clk or negedge rstn_async) begin
+    if(!rstn_async) rstn_d1 <= 1'b0;
     else rstn_d1 <= rstn_d0;
 end
 assign rstn_sync = rstn_d1;
