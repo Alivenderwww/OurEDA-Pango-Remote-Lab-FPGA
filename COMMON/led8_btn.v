bbba2529ab6bab19bb1b5687bbb2c9e7c7dcae01
alivender <13898766233@163.com>
2025-03-31 13:55:51 +0800

    对按键输入进行了消抖

--- COMMON/led8_btn.v
+++ COMMON/led8_btn.v
@@ -12,23 +12,39 @@ module led8_btn(
 );
 
 reg btn_up_d0, btn_up_d1;
+reg [5:0] cnt0;
 wire btn_up_neg = (~btn_up_d0) & (btn_up_d1);
+wire btn_up_neg_d = (cnt0 == 6'b111111);
+always @(posedge clk) begin
+    if(~rstn) cnt0 <= 0;
+    else if(cnt0==0 && btn_up_neg_d) cnt0 <= cnt0 + 1;
+    else if(cnt0!=0) cnt0 <= cnt0 + 1;
+    else cnt0 <= 0;
+end
+
+reg btn_down_d0, btn_down_d1;
+reg [5:0] cnt1;
+wire btn_down_neg = (~btn_down_d0) & (btn_down_d1);
+wire btn_down_neg_d = (cnt1 == 6'b111111);
 always @(posedge clk) begin
     btn_up_d0 <= btn_up;
     btn_up_d1 <= btn_up_d0;
 end
-
-reg btn_down_d0, btn_down_d1;
-wire btn_down_neg = (~btn_down_d0) & (btn_down_d1);
 always @(posedge clk) begin
     btn_down_d0 <= btn_down;
     btn_down_d1 <= btn_down_d0;
 end
+always @(posedge clk) begin
+    if(~rstn) cnt1 <= 0;
+    else if(cnt1==0 && btn_up_neg_d) cnt1 <= cnt1 + 1;
+    else if(cnt1!=0) cnt1 <= cnt1 + 1;
+    else cnt1 <= 0;
+end
 
 always @(posedge clk) begin
     if(~rstn) bcd <= 0;
-    else if(btn_up_neg) bcd <= bcd + 1;
-    else if(btn_down_neg) bcd <= bcd - 1;
+    else if(btn_up_neg_d) bcd <= bcd + 1;
+    else if(btn_down_neg_d) bcd <= bcd - 1;
     else bcd <= bcd;
 end
 
