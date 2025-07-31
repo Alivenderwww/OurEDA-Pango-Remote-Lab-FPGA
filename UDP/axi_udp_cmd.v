6ca7b444a709a1a2592f0334d293bcc880e966fd
DLUTdky <1737911955@qq.com>
2025-07-31 15:11:29 +0800

    修改debugger读取长度不能过小的问题

--- UDP/axi_udp_cmd.v
+++ UDP/axi_udp_cmd.v
@@ -7,30 +7,30 @@ module axi_udp_cmd  (
     output wire        MASTER_CLK          , //向AXI总线提供的本主机时钟信号
     output wire        MASTER_RSTN         , //向AXI总线提供的本主机复位信号
 
-    output wire [ 1:0] MASTER_WR_ADDR_ID   , //写地址通道-ID
-    output wire [31:0] MASTER_WR_ADDR      , //写地址通道-地址
-    output wire [ 7:0] MASTER_WR_ADDR_LEN  , //写地址通道-突发长度-最小为0（1突发），最大为255（256突发）
-    output wire [ 1:0] MASTER_WR_ADDR_BURST, //写地址通道-突发类型
-    output reg         MASTER_WR_ADDR_VALID, //写地址通道-握手信号-有效
-    input  wire        MASTER_WR_ADDR_READY, //写地址通道-握手信号-准备
+    output wire [ 1:0] MASTER_WR_ADDR_ID   /* synthesis PAP_MARK_DEBUG="true" */, //写地址通道-ID
+    output wire [31:0] MASTER_WR_ADDR      /* synthesis PAP_MARK_DEBUG="true" */, //写地址通道-地址
+    output wire [ 7:0] MASTER_WR_ADDR_LEN  /* synthesis PAP_MARK_DEBUG="true" */, //写地址通道-突发长度-最小为0（1突发），最大为255（256突发）
+    output wire [ 1:0] MASTER_WR_ADDR_BURST/* synthesis PAP_MARK_DEBUG="true" */, //写地址通道-突发类型
+    output reg         MASTER_WR_ADDR_VALID/* synthesis PAP_MARK_DEBUG="true" */, //写地址通道-握手信号-有效
+    input  wire        MASTER_WR_ADDR_READY/* synthesis PAP_MARK_DEBUG="true" */, //写地址通道-握手信号-准备
 
-    output wire [31:0] MASTER_WR_DATA      , //写数据通道-数据
-    output reg  [ 3:0] MASTER_WR_STRB      , //写数据通道-选通
-    output wire        MASTER_WR_DATA_LAST , //写数据通道-last信号
-    output wire        MASTER_WR_DATA_VALID, //写数据通道-握手信号-有效
-    input  wire        MASTER_WR_DATA_READY, //写数据通道-握手信号-准备
+    output wire [31:0] MASTER_WR_DATA      /* synthesis PAP_MARK_DEBUG="true" */, //写数据通道-数据
+    output reg  [ 3:0] MASTER_WR_STRB      /* synthesis PAP_MARK_DEBUG="true" */, //写数据通道-选通
+    output wire        MASTER_WR_DATA_LAST /* synthesis PAP_MARK_DEBUG="true" */, //写数据通道-last信号
+    output wire        MASTER_WR_DATA_VALID/* synthesis PAP_MARK_DEBUG="true" */, //写数据通道-握手信号-有效
+    input  wire        MASTER_WR_DATA_READY/* synthesis PAP_MARK_DEBUG="true" */, //写数据通道-握手信号-准备
 
     input  wire [ 1:0] MASTER_WR_BACK_ID    /* synthesis PAP_MARK_DEBUG="true" */, //写响应通道-ID
     input  wire [ 1:0] MASTER_WR_BACK_RESP  /* synthesis PAP_MARK_DEBUG="true" */, //写响应通道-响应
     input  wire        MASTER_WR_BACK_VALID /* synthesis PAP_MARK_DEBUG="true" */, //写响应通道-握手信号-有效
     output reg         MASTER_WR_BACK_READY /* synthesis PAP_MARK_DEBUG="true" */, //写响应通道-握手信号-准备
 
-    output wire [ 1:0] MASTER_RD_ADDR_ID   , //读地址通道-ID
-    output wire [31:0] MASTER_RD_ADDR      , //读地址通道-地址
-    output wire [ 7:0] MASTER_RD_ADDR_LEN  , //读地址通道-突发长度。最小为0（1突发），最大为255（256突发）
-    output wire [ 1:0] MASTER_RD_ADDR_BURST, //读地址通道-突发类型。
-    output reg         MASTER_RD_ADDR_VALID, //读地址通道-握手信号-有效
-    input  wire        MASTER_RD_ADDR_READY, //读地址通道-握手信号-准备
+    output wire [ 1:0] MASTER_RD_ADDR_ID   /* synthesis PAP_MARK_DEBUG="true" */, //读地址通道-ID
+    output wire [31:0] MASTER_RD_ADDR      /* synthesis PAP_MARK_DEBUG="true" */, //读地址通道-地址
+    output wire [ 7:0] MASTER_RD_ADDR_LEN  /* synthesis PAP_MARK_DEBUG="true" */, //读地址通道-突发长度。最小为0（1突发），最大为255（256突发）
+    output wire [ 1:0] MASTER_RD_ADDR_BURST/* synthesis PAP_MARK_DEBUG="true" */, //读地址通道-突发类型。
+    output reg         MASTER_RD_ADDR_VALID/* synthesis PAP_MARK_DEBUG="true" */, //读地址通道-握手信号-有效
+    input  wire        MASTER_RD_ADDR_READY/* synthesis PAP_MARK_DEBUG="true" */, //读地址通道-握手信号-准备
 
     input  wire [ 1:0] MASTER_RD_BACK_ID   /* synthesis PAP_MARK_DEBUG="true" */, //读数据通道-ID
     input  wire [31:0] MASTER_RD_DATA      /* synthesis PAP_MARK_DEBUG="true" */, //读数据通道-数据
